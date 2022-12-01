c3d8f79366887045387896230a6317cbf169b6bf
Peter Baumhart <peter.baumhart@gmail.com>
2022-11-30 23:46:43 -0600

    Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var calorieSumList = new List<long>();
+
+long sum = 0;
+
+foreach (var i in File.ReadAllLines("input.txt"))
+{
+    if (string.IsNullOrWhiteSpace(i))
+    {
+        calorieSumList.Add(sum);
+        sum = 0;
+    }
+    else
+    {
+        sum += long.Parse(i);
+    }
+}
+
+Console.WriteLine($"Part 1: {calorieSumList.OrderDescending().First()}");
+Console.WriteLine($"Part 2: {calorieSumList.OrderDescending().Take(3).Sum()}");
