a35ad7ce0e57c98ce820ac559e4c2581fb733b9f
Peter Baumhart <peter.baumhart@gmail.com>
2022-12-03 23:30:16 -0600

    Day 4

---
+++ Day04/Program.cs
@@ -0,0 +1,22 @@
+﻿var pairs = File.ReadAllLines("input.txt")
+    .Select(p => p.Split(',')
+        .Select(r => r.Split('-')
+            .Select(i => int.Parse(i)).ToList())
+        .ToList());
+
+Console.WriteLine($"Part 1: {pairs.Count(pair =>
+{
+    return (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) ||
+        (pair[1][0] >= pair[0][0] && pair[1][1] <= pair[0][1]);
+})}");
+
+Console.WriteLine($"Part 2: {pairs.Count(pair =>
+{
+    return (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) ||
+        (pair[1][0] >= pair[0][0] && pair[1][1] <= pair[0][1]) ||
+        (pair[0][0] <= pair[1][0] && pair[1][0] <= pair[0][1]) ||
+        (pair[1][0] <= pair[0][0] && pair[0][0] <= pair[1][1]);
+})}");
+
+
+
