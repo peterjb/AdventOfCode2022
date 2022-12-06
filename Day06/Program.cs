6f8bbf4f9b39ca85c667b5e98e4d405fe7f720c2
Peter Baumhart <peter.baumhart@gmail.com>
2022-12-05 23:26:00 -0600

    Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = File.ReadAllLines("input.txt").ToList()[0];
+
+Console.WriteLine($"Part 1: {findMarker(input, 4)}");
+Console.WriteLine($"Part 2: {findMarker(input, 14)}");
+
+int findMarker(string input, int lengthOfMarker)
+{
+    for (var i = 0; i < input.Length - lengthOfMarker; i++)
+    {
+        var marker = new HashSet<char>(input.Skip(i).Take(lengthOfMarker));
+        if (marker.Count == lengthOfMarker)
+        {
+            return i + lengthOfMarker;
+        }
+    }
+    throw new Exception("Whoops");
+}
