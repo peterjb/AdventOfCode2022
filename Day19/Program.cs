135249fc2852df4ff1a1cccd423ff5a7cb187c17
Peter Baumhart <peter.baumhart@gmail.com>
2022-12-20 01:13:01 -0600

    Day 20 - missing both 19, part 2 16 and 17

---
+++ Day19/Program.cs
@@ -0,0 +1,228 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("test.txt");
+
+var blueprints = new List<Blueprint>();
+
+
+foreach (var l in lines)
+{
+    var ms = Regex.Matches(l, @"\d+");
+    blueprints.Add(new Blueprint(ms.Select(m => int.Parse(m.Value)).ToList()));
+}
+
+var x = 100;
+var blueprintid = 0;
+
+Dictionary<State, State> stateCache = new();
+char[] robotTypes = new char[]
+{
+    'o', 'c', 's','g'
+};
+
+var results = new List<int>();
+
+for (var bs = 0; bs < blueprints.Count(); bs++)
+{
+    blueprintid = bs;
+    var g = doWork();
+    var bg = (bs + 1) * g;
+    results.Add(bg);
+    Console.WriteLine($"Blueprint {bs + 1} => {g} => {bg}");
+}
+
+Console.WriteLine(results.Sum());
+
+int doWork()
+{
+    stateCache = new();
+    var state = new State() { minute = 0, oreMachines = 1 };
+    var r = workHelper(state);
+    //Console.WriteLine("hi");
+    return r.geodes;
+}
+
+
+State workHelper(State state)
+{
+    if (stateCache.ContainsKey(state))
+    {
+        return stateCache[state];
+    }
+
+
+    if (state.clay < 0)
+        throw new Exception();
+
+    var nextState = state with
+    {
+        minute = state.minute + 1,
+        ore = state.ore + state.oreMachines,
+        clay = state.clay + state.clayMachines,
+        obsidian = state.obsidian + state.obsidianMachines,
+        geodes = state.geodes + state.geodeMachines
+    };
+
+    if (nextState.minute == 24)
+    {
+        return nextState;
+    }
+    var x = 24 - state.minute;
+    var b = blueprints[blueprintid];
+
+    int minTimeForClayMachine = (int)Math.Ceiling(Math.Max(b.clayOre - state.ore, 0) / (double)state.oreMachines) + 1;
+    int minTimeForObsidianMachine = int.MaxValue;
+    if(state.clayMachines == 0)
+    {
+       minTimeForObsidianMachine = Math.Max(
+           minTimeForClayMachine + b.obsidianClay,
+           (int)Math.Ceiling(Math.Max(b.obsidianOre - state.ore, 0) / (double)state.oreMachines)) + 1;
+    }
+    else
+    {
+        minTimeForObsidianMachine = Math.Max(
+           (int)Math.Ceiling(Math.Max(b.obsidianClay - state.clay, 0) / (double)state.clayMachines),
+           (int)Math.Ceiling(Math.Max(b.obsidianOre - state.ore, 0) / (double)state.oreMachines)) + 1;
+    }
+    int minTimeForGeodeMachine = int.MaxValue;
+    if(state.obsidianMachines == 0)
+    {
+        minTimeForGeodeMachine = Math.Max(
+            minTimeForObsidianMachine + b.geodeObsidian,
+            (int)Math.Ceiling(Math.Max(b.geodeOre - state.ore, 0) / (double)state.oreMachines)) + 1;
+    }
+    else
+    {
+        minTimeForGeodeMachine = Math.Max(
+            (int)Math.Ceiling(Math.Max(b.geodeObsidian - state.obsidian, 0) / (double)state.obsidianMachines),
+            (int)Math.Ceiling(Math.Max(b.geodeOre - state.ore, 0) / (double)state.oreMachines)) + 1;
+    }
+
+
+    if (x < minTimeForGeodeMachine)
+        //(state.geodeMachines == 0 && 
+        //(state.obsidianMachines == 0 && x < b.geodeObsidian ||
+        //state.obsidianMachines != 0 && x < Math.Max(Math.Max(0, b.geodeObsidian - state.obsidian)/ (float)state.obsidianMachines, Math.Max(0, (b.geodeOre - state.ore))/(float)state.oreMachines)) 
+        //|| (state.obsidianMachines == 0 && state.clayMachines != 0 && x <= b.geodeObsidian + Math.Max(Math.Max(0, b.obsidianClay - state.clay)/(float)state.clayMachines, Math.Max(0, b.obsidianOre - state.ore)/(float)state.oreMachines))
+        ////||
+        ////(x - b.geodeObsidian - b.obsidianClay < Math.Max(0, (b.clayOre - state.ore)) && state.clayMachines == 0)
+        //)
+    {
+        return nextState;
+    }
+
+    //if (x < (b.geodeObsidian - state.obsidian) + (b.geodeOre - state.ore)
+    //    && state.geodeMachines == 0)
+    //{
+    //    return nextState;
+    //}
+
+    var result = workHelper(nextState);
+    foreach (var c in robotTypes)
+    {
+
+        if (canCreateRobot(blueprintid, c, state))
+        {
+            if (c == 's')
+            {
+                var alkj = 100;
+            }
+            var t = workHelper(createRobot(blueprintid, c, nextState));
+            if (t.geodes > result.geodes)
+            {
+                result = t;
+            }
+        }
+    }
+    if (!stateCache.ContainsKey(state))
+        stateCache.Add(state, result);
+    else
+    {
+        var y = 1200;
+    }
+
+    return result;
+}
+
+
+bool canCreateRobot(int blueprintId, char robotType, State state)
+{
+    switch (robotType)
+    {
+        case 'o':
+            return state.ore >= blueprints[blueprintid].oreOre;
+        case 'c':
+            return state.ore >= blueprints[blueprintid].clayOre;
+        case 's':
+            return state.ore >= blueprints[blueprintid].obsidianOre && state.clay >= blueprints[blueprintid].obsidianClay;
+        case 'g':
+            return state.ore >= blueprints[blueprintid].geodeOre && state.obsidian >= blueprints[blueprintid].geodeObsidian;
+        default:
+            return false;
+    }
+}
+
+State createRobot(int bluePrintId, char robotType, State state)
+{
+    switch (robotType)
+    {
+        case 'o':
+            state.ore -= blueprints[blueprintid].oreOre;
+            state.oreMachines++;
+            break;
+        case 'c':
+            state.ore -= blueprints[blueprintid].clayOre;
+            state.clayMachines++;
+            break;
+        case 's':
+            state.ore -= blueprints[blueprintid].obsidianOre;
+            state.clay -= blueprints[blueprintid].obsidianClay;
+            state.obsidianMachines++;
+            break;
+        case 'g':
+            state.ore -= blueprints[blueprintid].geodeOre;
+            state.obsidian -= blueprints[blueprintid].geodeObsidian;
+            state.geodeMachines++;
+            break;
+        default:
+            break;
+    }
+
+    return state;
+}
+
+public record struct State(
+    int minute,
+    int ore,
+    int clay,
+    int obsidian,
+    int geodes,
+    int oreMachines,
+    int clayMachines,
+    int obsidianMachines,
+    int geodeMachines
+);
+
+struct Blueprint
+{
+    public Blueprint(List<int> values)
+    {
+        id = values[0];
+        oreOre = values[1];
+        clayOre = values[2];
+        obsidianOre = values[3];
+        obsidianClay = values[4];
+        geodeOre = values[5];
+        geodeObsidian = values[6];
+    }
+
+    public int id;
+    public int oreOre;
+    public int clayOre;
+    public int obsidianOre;
+    public int obsidianClay;
+    public int geodeOre;
+    public int geodeObsidian;
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.WriteLine($"Part 1: {decrypt(1, 1)}");
+Console.WriteLine($"Part 2: {decrypt(811589153, 10)}");
+
+long decrypt(long key, int times)
+{
+    var a = File.ReadAllLines("input.txt").Select((x, i) => (i, long.Parse(x) * key)).ToArray();
+    var length = a.Length;
+
+    var ll = new LinkedList<(int index, long value)>(a);
+    var d = new Dictionary<(int index, long value), LinkedListNode<(int index, long value)>>();
+
+    var n = ll.First;
+    while (n != null)
+    {
+        d.Add(n.Value, n);
+        n = n.Next;
+    }
+    for (var ii = 0; ii < times; ii++)
+    {
+        foreach (var num in a)
+        {
+            var orig = d[num];
+            var node = orig;
+            var value = node.Value.value;
+
+            if (value < 0)
+            {
+                var l = Math.Abs(value) % (length - 1);
+
+                for (var i = 0; i < l; i++)
+                {
+                    node = node.Previous;
+                    if (node == null)
+                        node = ll.Last;
+                    if (i == 0)
+                        ll.Remove(orig);
+                }
+                d[orig.Value] = ll.AddBefore(node, orig.Value);
+            }
+            else if (value > 0)
+            {
+                var l = value % (length - 1);
+                for (var i = 0; i < l; i++)
+                {
+                    node = node.Next;
+                    if (node == null)
+                        node = ll.First;
+                    if (i == 0)
+                        ll.Remove(orig);
+                }
+                d[orig.Value] = ll.AddAfter(node, orig.Value);
+            }
+        }
+    }
+
+    var zero = d.Where(x => x.Value.Value.value == 0).First().Value;
+    long aa = go(ll, zero, 1000).value;
+    long bb = go(ll, zero, 2000).value;
+    long cc = go(ll, zero, 3000).value;
+    return aa + bb + cc;
+}
+
+
+T go<T>(LinkedList<T> list, LinkedListNode<T> start, int amount)
+{
+    var node = start;
+    for (var i = 0; i < amount; i++)
+    {
+        node = node.Next;
+        if (node == null)
+            node = list.First;
+    }
+    return node.Value;
+}
