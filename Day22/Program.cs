36dcf16aee9dbe985c4def1c1efa05b4efffd9f5
Peter Baumhart <peter.baumhart@gmail.com>
2022-12-30 17:14:35 -0600

    All done

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System.Text.RegularExpressions;
 
-var lines = File.ReadAllText("test.txt").Split("\r\n\r\n");
+
+/* Calling it good here. because i did part 2 a stupid way, i just have a 
+ * transformed r, c face and direction instead of the actual answer
+ * so there's a little extra work to be done to calculate the password.
+ */
+var lines = File.ReadAllText("input.txt").Split("\r\n\r\n");
 
 var map = lines[0].Split("\r\n").ToArray();
 
 var instructions = Regex.Matches(lines[1], @"((\d+)|(R|L))").Select(m => m.Value).ToArray();
-
+var cdirs = new char[] { '>', 'v', '<', '^' };
 var dirs = new List<(int c, int r)>()
 {
     (1,0),
@@ -14,110 +19,139 @@ var dirs = new List<(int c, int r)>()
     (0,-1)
 };
 
-var s = 4;
-
-var sides = new Dictionary<string, Side>()
+(string? name, string orientation)[,] inputFaceMap = new[,]
 {
-    {
-        "Top", new Side() { name = "Top", r1 = 0, r2 = s-1, c1 = s, c2 = 2*s-1  }
-    },
-    {
-        "Right", new Side() { name = "Right", r1 = 0, r2 = s-1, c1 = 2*s, c2 = 3*s-1 }
-    },
-    {
-        "Near", new Side() { name = "Near", r1 = s, r2 = 2*s-1, c1 = s, c2 = 2*s-1 }
-    },
-    {
-        "Bottom", new Side() { name = "Bottom", r1 = 2*s, r2 = 3*s-1, c1 = s, c2 = 2*s-1 }
-    },
-    {
-        "Left", new Side() { name = "Left",     r1 = 2*s, r2 = 3*s-1, c1 = 0, c2 = s-1 }
-    },
-    {
-        "Far", new Side() { name = "Far",       r1 = 3*s, r2 = 4*s-1, c1 = 0, c2 = s-1 }
-    },
+    { (null,null), ("top",null), ("right", "rotr"), (null,null) },
+    { (null,null), ("front",null), (null,null), (null,null) },
+    { ("left","rotr"), ("bottom","flipb"), (null,null), (null,null) },
+    { ("back","rotr"), (null,null), (null, null), (null, null) }
 };
 
-var sideRef = new Dictionary<string, List<string>>()
+(string? name, string? orientation)[,] testFaceMap = new[,]
 {
-    {"Top", new() {"Right", "Near", "Left", "Far" } },
-    {"Right", new() {"Bottom", "Near", "Top", "Far" } },
-    {"Near", new() {"Right", "Bottom", "Left", "Top" } },
-    {"Bottom", new() {"Right", "Far", "Left", "Near" } },
-    {"Left", new() {"Bottom", "Far", "Top", "Near" } },
-    {"Far", new() {"Bottom", "Right", "Top", "Left" } },
+    { (null,null), (null,null), ("top", null), (null,null) },
+    { ("back",null), ("left",null), ("front",null), (null,null) },
+    { (null,null), (null,null), ("bottom","flipb"), ("right","rotl") },
+    { (null,null), (null,null), (null, null), (null, null) }
 };
 
 
-Dictionary<string, List<string>> naturalFaces = new()
-{
-    {"Near", new() { "Right", "Bottom", "Left", "Top" } },
-    {"Top", new() { "Right", "Near", "Left", "Far" } },
-    {"Right", new() { "Far", "Bottom", "Near", "Top" } },
-    {"Bottom", new() { "Right", "Far", "Left", "Near" } },
-    {"Left", new() { "Right", "Bottom", "Left", "Top" } },
-    {"Far", new() { "Right", "Bottom", "Left", "Top" } },
-};
+var s = 50;
+(string? name, string? orientation)[,] faceMap = inputFaceMap;// testFaceMap;
 
-
-var faceGrid = new Face[4, 4];
-
+var faces = new Dictionary<string, Face>();
 for (var i = 0; i < map.Length; i += s)
 {
     var fr = i / s;
     var start = map[i].ToList().FindIndex(c => c != ' ');
+    if (start == -1)
+        continue;
+
     for (var j = start; j < map[i].Length; j += s)
     {
         var fc = j / s;
-        faceGrid[fr, fc] = new Face() { grid = map.Skip(i).Take(s).Select(str => str.Substring(j, s)).ToArray() };
-    }
-}
+        var o = faceMap[fr, fc];
+        var grid = map.Skip(i).Take(s).Select(str => str.Substring(j, s)).ToArray();
+        var v = new Face() { name = o.name, grid = new char[s, s] };
 
-
-HashSet<(int r, int c)> visited = new();
-
-Face[] FindOrder() {
-    for (var r = 0; r < 4; r++)
-    {
-        for (var c = 0; c < 4; c++)
+        for (var rr = 0; rr < s; rr++)
         {
-            if (faceGrid[r,c] == null) continue;    
-
-            visited = new();
-            var order = GetOrder((r, c), new());
-            if (order != null)
-                return order;
+            for (var cc = 0; cc < s; cc++)
+            {
+                switch (o.orientation)
+                {
+                    case "fliph":
+                        v.grid[rr, cc] = grid[rr][s - cc - 1];
+                        break;
+                    case "flipv":
+                        v.grid[rr, cc] = grid[s - rr - 1][cc];
+                        break;
+                    case "flipb":
+                        v.grid[rr, cc] = grid[s - rr - 1][s - cc - 1];
+                        break;
+                    case "rotr":
+                        v.grid[cc, s - rr - 1] = grid[rr][cc];
+                        break;
+                    case "rotl":
+                        v.grid[s-cc-1, rr] = grid[rr][cc];
+                        break;
+                    default:
+                        v.grid[rr, cc] = grid[rr][cc];
+                        break;
+                }
+            }
         }
+        faces[o.name] = v;
     }
-    return null;
 }
 
 
-
-string[]? faceNames = new string[] { "front", "top", "back", "bottom", "left", "right" };
-int orientations = 4;
-var moves = new (int y, int x)[] { (0, 1), (1, 0), (0, -1), (-1, 0) };
-Face[] GetOrder((int r, int c) pos, HashSet<string> faces)
+Dictionary<string, List<(string n, Func<int, int, (int, int)> t, int d)>> naturalFaces = new()
 {
-    throw new NotImplementedException();
-    //if(faces.Count() == 6)
-    //    return Array.Empty<Face>();
+    {
+        "front", new() {
+            (n: "right" , t: (int r, int c) => (r, 0), 0),
+            (n: "bottom", t: (int r, int c) => (s - 1, s - c - 1), 3),
+            (n: "left"  , t: (int r, int c) => (r, s - 1), 2),
+            (n: "top"   , t: (int r, int c) => (s - 1, c), 3)
+        }
+    },
+    {
+        "top", new() {
+            ("right", (int r, int c) => (0, s - r - 1), 1),
+            ("front", (int r, int c) => (0, c), 1),
+            ("left" , (int r, int c) => (0, r), 1),
+            ("back" , (int r, int c) =>(0, s - c - 1) , 1)
+        }
+    },
+    {
+        "right" , new() {
+            ("back"   , (int r, int c) => (r,0), 0),
+            ("bottom" , (int r, int c) => (s-c-1, 0), 0),
+            ("front"  , (int r, int c) => (r, s - 1), 2),
+            ("top"    , (int r, int c) => (s -c - 1, s - 1), 2)
+        }
+    },
+    {
+    "bottom", new() {
+        ("left", (int r, int c) => (s-1,r), 3),
+        ("front" , (int r, int c) => (s-1,s-c-1), 3),
+        ("right" , (int r, int c) => (s-1,s-r-1), 3),
+        ("back" , (int r, int c) => (s-1, c), 3)
+    }
+    },
+    {
+    "left", new() {
+        ("front", (int r, int c) => (r,0), 0),
+        ("bottom", (int r, int c) => (c,s-1), 2),
+        ("back" , (int r, int c) => (r,s-1), 2),
+        ("top"   , (int r, int c) => (c,0), 0)
+    }
+    },
+    {
+    "back", new() {
+        ("left", (int r, int c) => (r,0), 0),
+        ("bottom", (int r, int c) => (0,c), 1),
+        ("right" , (int r, int c) => (r,s-1), 2),
+        ("top"   , (int r, int c) => (0, s - c - 1), 1)
+    }
+    },
+};
 
-    //foreach(var m in moves)
-    //{
-    //    var next = (r: pos.r + m.y, c: pos.c + m.x);
-    //    if(next.r < 4 && next.r >= 0 && next.c < 4 && next.c >= 0)
-    //    {
-    //        var result = GetOrder(next, faces);
-    //    }
-    //}
-}
-
-
-
+//setup part 1
 //r not in set
 var rbounds = new Dictionary<int, (int left, int right)>();
 var cbounds = new Dictionary<int, (int top, int bottom)>();
+
+Dictionary<string, Dictionary<string, bool>> seen = new();
+foreach(var x in faces)
+{
+    seen.Add(x.Key, new());
+    foreach(var y in faces)
+    {
+        seen[x.Key].Add(y.Key, x.Key == y.Key);
+    }
+}
 
 for (var row = 0; row < map.Length; row++)
 {
@@ -182,24 +216,50 @@ Console.WriteLine($"Part 1: {1000 * (r + 1) + 4 * (c + 1) + dir}");
 
 
 
-
+var doprint = false;
 dir = 0; //right
 r = 0;
-c = rbounds[0].left;
-var side = sides["Top"];
-
+c = 0;
+var face = faces["top"];
+int printcount = 0;
+int totalprintcount = 0;
 for (var ii = 0; ii < instructions.Length; ii += 2)
 {
     var dist = int.Parse(instructions[ii]);
-
+    //if(true)
+    //{
+    //    totalprintcount++;
+    //    print();
+    //    Console.ReadKey(); printcount++;
+    //    //if (printcount > 1)
+    //    //{
+    //    //    doprint = false;
+    //    //    printcount = 0;
+    //    //}
+    //}
     for (var mi = 0; mi < dist; mi++)
     {
-        var (newr, newc, newdir) = moveWithWrap(r, c, dir);
-
-        if (map[newr][newc] == '#')
+        var (f, (newr, newc), newdir) = moveWithWrap(face, r, c, dir);
+        
+        if (f.grid[newr, newc] == '#')
             break;
         else
         {
+            //if (!seen[face.name][f.name] || doprint)
+            //{
+            //    seen[face.name][f.name] = true;
+            //    totalprintcount++;
+            //    doprint = true;
+                //print();
+                //printcount++;
+                //Console.ReadKey();
+            //    if (printcount > 1)
+            //    {
+            //        doprint = false;
+            //        printcount = 0;
+            //    }
+            //}
+            face = f;
             r = newr;
             c = newc;
             dir = newdir;
@@ -213,7 +273,7 @@ for (var ii = 0; ii < instructions.Length; ii += 2)
     }
 }
 
-Console.WriteLine($"Part 2: {1000 * (r + 1) + 4 * (c + 1) + dir}");
+Console.WriteLine($"Part 2: {face.name}: ({r + 1},{c + 1}) Direction: {dir}");
 
 int rotate(string d)
 {
@@ -228,119 +288,48 @@ int rotate(string d)
     }
 }
 
-(int r, int c, int d, Side s) moveWithWarp2(int r, int c, int dir, Side s)
+void print()
 {
-    bool newSide = false;
-    Side nextSide = sides[sideRef[s.name][dir]];
-
-    if ((r == s.r1 && dir == 3 && nextSide.r2 != s.r1 - 1 && nextSide.c1 != s.c1) ||
-        (r == s.r2 && dir == 1 && nextSide.r1 != s.r2 + 1 && nextSide.c1 != s.c1) ||
-        (c == s.c1 && dir == 2 && nextSide.c2 != s.c1 - 1 && nextSide.r1 != s.r1) ||
-        (c == s.c2 && dir == 0 && nextSide.c1 != s.c2 + 1 && nextSide.r1 != s.r1)
-    )
+    foreach (var f in faces)
     {
-        newSide = true;
-
-        int sr = sideRef[nextSide.name].FindIndex(str => str == s.name);
-
-        //calc new pos
+        if (f.Value.name != face.name)
+            continue;
+        Console.WriteLine($"{totalprintcount}: {f.Key}: ({r},{c}): {dir}");
+        for (var row = 0; row < s; row++)
+        {
+            for (var col = 0; col < s; col++)
+            {
+                if (f.Key == face.name && r == row && c == col)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Write(cdirs[dir]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else 
+                {
+                    Console.Write(f.Value.grid[row, col]);
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
     }
-
-    var delta = dirs[dir];
-
-    return (r + delta.r, c + delta.c, dir, newSide ? nextSide : s);
 }
 
-(int r, int c, int d) moveWithWrap(int r, int c, int dir)
+(Face f, (int r, int c), int d) moveWithWrap(Face f, int r, int c, int d)
 {
-    //top to far
-    if (r == 0 && dir == 3)
+    var nf = naturalFaces[f.name][d];
+    //up, down, left, right
+    if ((r == 0 && d == 3) || (r == s - 1 && d == 1) || (c == 0 && d == 2) || (c == s - 1 && d == 0))
     {
-        return (s, 3 * s - c - 1, 1);
-    }
-
-    //far to top
-    if (r == s + 1 && c < 2 && dir == 3)
-    {
-        return (0, 3 * s + c - 1, 1);
-    }
-
-    //bottom to far
-    if (r == 3 * s - 1 && dir == 1)
-    {
-        return (2 * s - 1, 3 * s - c - 1, 3);
-
-    }
-
-    //far to bottom
-    if (r == 2 * s - 1 && c < 2 && dir == 1)
-    {
-        return (3 * s - 1, 3 * s + c - 1, 3);
-
-    }
-
-    //top to left
-    if (c == 2 * s && r < s && dir == 2)
-    {
-        return (2, r + s, 1);
-    }
-
-    //left to top
-    if (r == s && c >= s && c < 2 * s && dir == 3)
-    {
-        return (c - s, 2 * s, 0);
-    }
-
-    //bottom to left
-    if (c == 2 * s && r >= 2 * s && dir == 2)
-    {
-        return (2 * s - 1, -r + 4 * s - 1, 3);
-    }
-
-    //left to bottom
-    if (r == (2 * s - 1) && c >= s && c < 2 * s && dir == 1)
-    {
-        return (-c + 4 * s - 1, 2 * s, 0);
-    }
-
-    //near to right
-    if (c == 3 * s - 1 && r >= s && r < 2 * s && dir == 0)
-    {
-        return (2 * s, -r + 5 * s - 1, 1);
-    }
-
-    //right to near
-    if (r == 2 * s && c >= 3 * s && dir == 3)
-    {
-        return (-c + 5 * s - 1, 3 * s - 1, 2);
-    }
-
-    //top to right
-    if (c == 3 * s - 1 && r < s && dir == 0)
-    {
-        return (-r + 3 * s - 1, 4 * s - 1, 2);
-    }
-
-    //right to top
-    if (c == 4 * s - 1 && r >= 2 * s && dir == 0)
-    {
-        return (-r + 3 * s - 1, 3 * s - 1, 2);
-    }
-
-    //far to right
-    if (c == 0 && r >= s && r < 2 * s && dir == 2)
-    {
-        return (3 * s - 1, -r + 5 * s - 1, 3);
-    }
-
-    if (r == 3 * s - 1 && c >= 3 * s && dir == 1)
-    {
-        return (-r + 5 * s - 1, 0, 0);
+        return (faces[nf.n], nf.t(r, c), nf.d);
     }
 
     var delta = dirs[dir];
 
-    return (r + delta.r, c + delta.c, dir);
+    return (f, (r + delta.r, c + delta.c), d);
 }
 
 
@@ -356,7 +345,7 @@ struct Side
 class Face
 {
     public string name;
-    public string[] grid;
+    public char[,] grid;
     public string? top;
     public string? right;
     public string? bottom;
